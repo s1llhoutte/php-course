7b93252217c84fef77cdfe51956effa2b24e77f7
s1llhoutte <danikval2000002@gmail.com>
2024-06-13 13:56:10 +0300

    mnogomernie massive

--- index.php
+++ index.php
@@ -1,14 +1,33 @@
 <?php
-$person = [
-    'name' => 'Victor',
-    'age' => 20,
-    'hobby' => 'swimming',
-    'is_married' => true,
-    'pet' => 'cat',
-    'pet_name' => 'Barsik'
+
+$persons = [
+    [
+        'name' => 'Victor',
+        'age' => 20,
+        'hobby' => 'swimming',
+        'is_married' => true,
+        'pet' => 'cat',
+        'pet_name' => 'Barsik',
+        'cars' => ['jigul', 'moskich', 'volga']
+    ],
+    [
+        'name' => 'Karl',
+        'age' => 20,
+        'hobby' => 'swimming',
+        'is_married' => true,
+        'pet' => 'cat',
+        'pet_name' => 'Barsik'
+    ],
+    [
+        'name' => 'Margo',
+        'age' => 18,
+        'hobby' => 'swimming',
+        'is_married' => true,
+        'pet' => 'cat',
+        'pet_name' => 'Barsik'
+    ]
 ];
 
-foreach ($person as $key => $item) {
-    echo $key . ': ';
-    echo $item . "\n";
+foreach ($persons as $person) {
+    print_r($person['name']);
 }
