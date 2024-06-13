caa13439491408582531498758ce5893212c05bb
s1llhoutte <danikval2000002@gmail.com>
2024-06-13 13:15:29 +0300

    massive php

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
 <?php
-$mame = 'Victor';
-$age = 20;
-$hobby = 'swimming';
-$isMarried = true;
-$pet = 'cat';
-$petName = 'Barsik';
+$person = [
+    'name' => 'Victor',
+    'age' => 20,
+    'hobby' => 'swimming',
+    'is_married' => true,
+    'pet' => 'cat',
+    'pet_name' => 'Barsik'
+];
 
-echo $mame . "\n";
-echo $age . "\n";
-echo $hobby . "\n";
-echo $isMarried . "\n";
-echo $pet . "\n";
-echo $petName . "\n";
+print_r($person['name']);
+
+
+$secondPerson = [
+    'name' => 'Vasya',
+    'age' => 20
+];
+
+$secondPerson['hobby'] = 'swimming';
+print_r($secondPerson);
+
+unset($secondPerson['name']);
+
+print_r($secondPerson);
