3b6a45a7ee74e5f3d4315044bc764b3eeda94388
s1llhoutte <danikval2000002@gmail.com>
2024-06-13 15:16:22 +0300

    function php

--- index.php
+++ index.php
@@ -1,46 +1,9 @@
 <?php
 
-$persons = [
-    [
-        'name' => 'Victor',
-        'age' => 20,
-        'hobby' => 'swimming',
-        'is_married' => true,
-        'pet' => 'cat',
-        'pet_name' => 'Barsik',
-        'cars' => ['jigul', 'moskich', 'volga']
-    ],
-    [
-        'name' => 'Karl',
-        'age' => 20,
-        'hobby' => 'swimming',
-        'is_married' => false,
-        'pet' => 'cat',
-        'pet_name' => 'Barsik'
-    ],
-    [
-        'name' => 'Margo',
-        'age' => 18,
-        'hobby' => 'swimming',
-        'is_married' => false,
-        'pet' => 'cat',
-        'pet_name' => 'Barsik'
-    ]
-];
-
-//foreach ($persons as $person) {
-//    if ($person['is_married']) {
-//        echo 'женат: ' . $person['name'] . "\n";
-//    } else {
-//        echo 'не женат: ' . $person['name'] . "\n";
-//    }
-//}
-
-$flat = '20';
-$name = 'Gena';
-
-if ($flat == 20 && $flat === 20) {
-    echo 'YES';
-} else {
-    echo 'NO';
+function sayHello() {
+    echo 'hello';
+    $name = 'Vasya';
+    echo $name;
 }
+
+sayHello();
