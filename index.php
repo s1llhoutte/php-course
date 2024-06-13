cbe5788e1e97d36e232f76a76333fa7b6cf9a472
s1llhoutte <danikval2000002@gmail.com>
2024-06-13 12:51:14 +0300

    peremennie php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo 'aaa';
+$mame = 'Victor';
+$age = 20;
+$hobby = 'swimming';
+$isMarried = true;
+$pet = 'cat';
+$petName = 'Barsik';
+
+echo $mame . "\n";
+echo $age . "\n";
+echo $hobby . "\n";
+echo $isMarried . "\n";
+echo $pet . "\n";
+echo $petName . "\n";
